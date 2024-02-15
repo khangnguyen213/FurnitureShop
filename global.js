a499b5a0135849b0ef2d69b61260fb8bc52af3d9
KhangNguyen <khangnguyenm01@gmail.com>
2024-02-15 21:29:19 +0700

    change server

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
 class Global {
   static BASE_URL = 'https://furniture-shop-eight.vercel.app';
-  static BASE_SERVER = 'https://server1.kansan.io.vn';
+  static BASE_SERVER = 'https://furniture-shop-be.vercel.app/';
 }
